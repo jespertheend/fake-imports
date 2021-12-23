2ef2c8261a8433b07e0dd7474ad28867353b3e5d
Jesper van den Ende <jespertheend@gmail.com>
2021-12-23 03:07:45 +0100

    Use deno for linting

--- src/parseImports.js
+++ src/parseImports.js
@@ -9,12 +9,13 @@
  * @param {string} scriptSource
  */
 export function parseImports(scriptSource) {
+	// deno-lint-ignore no-invalid-regexp no-empty-character-class
 	const re = /import[\s\S]+?["'](?<url>.+)["']/gd;
 	/** @type {ImportLocation[]} */
 	const imports = [];
 	for (const match of scriptSource.matchAll(re)) {
 		const url = match.groups?.url;
-		// @ts-ignore
+		// @ts-ignore indices does not exist but all browsers support it
 		const start = match.indices.groups?.url[0];
 		if (url && start !== undefined) {
 			imports.push({
